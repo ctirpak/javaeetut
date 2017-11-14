bbb0f2bea69e70776b77b863a376af4696d36860
Chris Tirpak <ctirpak1@gmail.com>
2017-11-14 12:52:59 -0500

    minor changes to db scripts and data

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -16,17 +16,6 @@ CREATE TABLE `product` (
   CONSTRAINT `fk_product_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='contains product details';
 
-CREATE TABLE `ordered_product` (
-  `customer_order_id` int(10) unsigned NOT NULL,
-  `product_id` int(10) unsigned NOT NULL,
-  `quantity` smallint(5) unsigned NOT NULL,
-  PRIMARY KEY (`customer_order_id`,`product_id`),
-  KEY `fk_customer_order_has_product_product1_idx` (`product_id`),
-  KEY `fk_customer_order_has_product_customer_order1_idx` (`customer_order_id`),
-  CONSTRAINT `fk_ordered_product_order` FOREIGN KEY (`customer_order_id`) REFERENCES `customer_order` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_ordered_product_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 CREATE TABLE `customer` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
@@ -48,4 +37,15 @@ CREATE TABLE `customer_order` (
   KEY `fk_customer_order_customer1_idx` (`customer_id`),
   CONSTRAINT `fk_customer_order_customer1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='maintains customer order details';
+
+CREATE TABLE `ordered_product` (
+  `customer_order_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `quantity` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`customer_order_id`,`product_id`),
+  KEY `fk_customer_order_has_product_product1_idx` (`product_id`),
+  KEY `fk_customer_order_has_product_customer_order1_idx` (`customer_order_id`),
+  CONSTRAINT `fk_ordered_product_order` FOREIGN KEY (`customer_order_id`) REFERENCES `customer_order` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ordered_product_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
